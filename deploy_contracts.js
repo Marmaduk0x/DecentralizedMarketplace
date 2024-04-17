f436533eff19b870f218da721f5468c645d0cf7c
Marma__duk <160778212+Marmaduk0x@users.noreply.github.com>
2024-04-17 20:11:43 +0200

    function added

--- deploy_contracts.js
+++ deploy_contracts.js
@@ -1,16 +1,26 @@
 const Marketplace = artifacts.require("Marketplace");
 require('dotenv').config();
 
-module.exports = async function(deployer, network) {
+module.exports = async function(deployer, network, accounts) {
   let provider, account;
 
-  if(network === 'development') {
+  if (network === 'development') {
     deployer.deploy(Marketplace);
-  } else if(network === 'production') {
+  } else if (network === 'production') {
     provider = process.env.PRODUCTION_PROVIDER;
-    account = process.env.PRODUCTION_ACCOUNT;
+    account = process.env.PRODUCTION_ACCOUNT || accounts[0]; // Fallback to the first account if not specified
 
-    deployer.deploy(Marketplace, {from: account});
+    await deployer.deploy(Marketplace, {from: account})
+      .then(() => {
+        if (process.env.INITIAL_FUNDING && process.env.INITIAL_FUNDING > 0) {
+          // Assuming Marketplace contract has a payable fallback function or a specific payable function to accept funds
+          return web3.eth.sendTransaction({
+            from: account,
+            to: Marketplace.address,
+            value: web3.utils.toWei(process.env.INITIAL_FUNDING, "ether"),
+          });
+        }
+      })
   } else {
     console.log("Please check your Truffle configuration for the network settings.");
   }
